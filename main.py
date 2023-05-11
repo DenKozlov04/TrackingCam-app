d9f235af22e4f6a336778e4de0eb65f5907b3940
Maksims Visockis <Vezun4ik2004@gmail.com>
2023-05-11 21:06:03 +0300

    color filter version

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
+#в данной версии мы используем алгоритм выделения обьекта с помощью цветового фильтра
+#(это делается для того, чтобы избежать ошибок если обьект большой и контрастный)
+#opencv - библиотека для обработки видеопотока с камеры и отображения на экране прямоугольника вокруг объекта определенного цвета.
+
 import cv2 as cv
 import numpy as np
 
-# img = cv.imread("images/test.png")
-# print (img.shape)
+# определяем камеру с помощью opencv
+vid = cv.VideoCapture(0)
 
+# определяем нижний и верхний цветовые пороги для фильтра
+lower_color = np.array([0, 50, 50])
+upper_color = np.array([10, 255, 255])
 
-# Укажите путь к изображению
-image_path = "images/test.png"
+# определяем начальные координаты и размеры прямоугольника
+x, y, w, h = 0, 0, 0, 0 
 
-# Загрузите изображение с помощью функции imread()
-image = cv.imread(image_path)
+while(True):
+    # берем видео с камеры
+    ret, frame = vid.read()
 
-# Проверьте, удалось ли загрузить изображение
-if image is not None:
-    # Показать изображение с помощью функции imshow()
-    cv.imshow("Изображение", image)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-else:
-    print("Не удалось загрузить изображение")
+    # преобразуем изображение в цветовое пространство HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # применяем цветовой фильтр к изображению
+    mask = cv.inRange(hsv, lower_color, upper_color)
+
+    # находим контуры объектов на изображении
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # выбираем контур с наибольшей площадью
+    if len(contours) > 0:
+        max_contour = max(contours, key=cv.contourArea)
+        x, y, w, h = cv.boundingRect(max_contour)
+
+    # рисуем прямоугольник на изображении
+    cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # отображаем
+    cv.imshow('frame', frame)
+
+    # проверяем, была ли нажата клавиша 'q'
+    if cv.waitKey(1) & 0xFF == ord('q') or cv.waitKey(1) & 0xFF == 27:
+        break
+
+    # проверяем, закрыта ли окно пользователем
+    if cv.getWindowProperty('frame', cv.WND_PROP_VISIBLE) < 1:
+        break
+
+# после цикла освобождаем камеру
+vid.release()
+
+# уничтожаем все окна
+cv.destroyAllWindows()
